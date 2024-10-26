6621aed8ac40596db449e7eb96bcef34ac46f348
JULIANA MENEZES DE JESUS <juliana.mjesus3@senacsp.edu.br>
2024-10-25 21:50:58 -0300

    Configurando o pool de conexões

--- app/carstore/src/main/java/br/com/carstore/servlet/dao/CarDao.java
+++ app/carstore/src/main/java/br/com/carstore/servlet/dao/CarDao.java
@@ -1,5 +1,6 @@
 package br.com.carstore.servlet.dao;
 
+import br.com.carstore.servlet.config.ConnectionPoolConfig;
 import br.com.carstore.servlet.model.Car;
 
 import java.sql.Connection;
@@ -18,9 +19,7 @@ public class CarDao {
 
         try {
 
-            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
-
-            System.out.println("Sucesso ao se conectar com o DB!");
+            Connection connection = ConnectionPoolConfig.getConnection();
 
             PreparedStatement preparedStatement = connection.prepareStatement(SQL);
 
@@ -46,9 +45,7 @@ public class CarDao {
 
         try {
 
-            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
-
-            System.out.println("Sucesso ao se conectar com o DB!");
+            Connection connection = ConnectionPoolConfig.getConnection();
 
             PreparedStatement preparedStatement = connection.prepareStatement(SQL);
 
@@ -90,9 +87,7 @@ public class CarDao {
 
         try {
 
-            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
-
-            System.out.println("success in database connection");
+            Connection connection = ConnectionPoolConfig.getConnection();
 
             PreparedStatement preparedStatement = connection.prepareStatement(SQL);
             preparedStatement.setString(1, carId);
@@ -116,9 +111,7 @@ public class CarDao {
 
         try {
 
-            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa","sa");
-
-            System.out.println("success in database connection");
+            Connection connection = ConnectionPoolConfig.getConnection();
 
             PreparedStatement preparedStatement = connection.prepareStatement(SQL);
 
